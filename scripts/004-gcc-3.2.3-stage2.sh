cbc743f15de1841c568190977ca1893726ebad36
BoGanon <michaelkey@gmail.com>
2018-05-26 21:08:15 -0400

    Forgot to add gcc-core to sources for gcc-stage2.

--- scripts/004-gcc-3.2.3-stage2.sh
+++ scripts/004-gcc-3.2.3-stage2.sh
@@ -4,6 +4,11 @@
 
 GCC_VERSION=3.2.3
 ## Download the source code.
+SOURCE=http://ftpmirror.gnu.org/gcc/gcc-$GCC_VERSION/gcc-core-$GCC_VERSION.tar.bz2
+if [ ! -e gcc-core-$GCC_VERSION.tar.bz2 ]; then
+	wget --continue $SOURCE || { exit 1; }
+fi
+
 SOURCE=http://ftpmirror.gnu.org/gcc/gcc-$GCC_VERSION/gcc-g++-$GCC_VERSION.tar.bz2
 if [ ! -e gcc-g++-$GCC_VERSION.tar.bz2 ]; then
 	wget --continue $SOURCE || { exit 1; }
